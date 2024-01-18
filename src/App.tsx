9730d6611802b6bf99cc0845e617106e3d0dda79
MikeJ0 <misha7709123@gmail.com>
2024-01-18 21:33:30 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <span>wwwwww</span>
+     Hello, samurai! Let's go!
+        lllllllwwwsssssssssqqqqqqqs
+    </div>
+
+  );
+}
+
+export default App;
